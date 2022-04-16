588553db5120a66dc71b2f09bca3840778fe3237
SangminPark <psmdc0714@gmail.com>
2022-04-16 20:56:47 +0900

    220416 solved
    This is the solved problem on 220416

---
+++ c++/15552.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t, a, b;
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> t;
+    for(int i=0; i<t; i++){
+        cin >> a >> b;
+        cout << a+b << "\n";
+    }
+
+    return 0;
+}
