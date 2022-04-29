086a909a69c7e3c6f9d4514ab06489db89ff3625
SangminPark <psmdc0714@gmail.com>
2022-04-29 17:50:51 +0900

    220429 solved
    This is the solved problem on 220429

---
+++ c++/10926.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> s;
+    cout << s << "\?\?!";
+
+    return 0;
+}
