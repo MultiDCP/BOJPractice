1c990bb12a27beb0e0cdadbb46eb34dfea491e1c
SangminPark <psmdc0714@gmail.com>
2022-05-27 23:25:43 +0900

    220527 solved
    This is the solved problem on 220527

---
+++ c++/3046.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r1, s;
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> r1 >> s;
+    cout << s * 2 - r1;
+
+    return 0;
+}
