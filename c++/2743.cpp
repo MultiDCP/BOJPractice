37df76558b9fcdc59cf70915826d90ced1ef6cfc
SangminPark <psmdc0714@gmail.com>
2022-06-18 18:54:47 +0900

    220617 solved
    This is the solved problem on 220617

---
+++ c++/2743.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+    cout << s.length();
+
+    return 0;
+}
