9fa0cc008329f1d0eb35be9da975db992dd3e489
SangminPark <psmdc0714@gmail.com>
2022-07-13 00:08:14 +0900

    220712 solved
    This is the solved problem on 220712... because of full schedules

---
+++ c++/11382.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long a, b, c;
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+    return 0;
+}
