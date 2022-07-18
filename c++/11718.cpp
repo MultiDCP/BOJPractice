180ea90358a59d99a672557abdd515f6da1dabc0
SangminPark <psmdc0714@gmail.com>
2022-07-18 23:56:25 +0900

    220718 solved
    This is the solved problem on 220718
    
    ...and too busy for solving harder problems.......... I'll do my best next time

---
+++ c++/11718.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while(true){
+        string s;
+        getline(cin, s);
+        if(s == "") break;
+        cout << s << "\n";
+    }
+
+    return 0;
+}
