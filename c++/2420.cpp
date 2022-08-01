5375e1a0f311e2d027990983b1dd9029a0552e3d
SangminPark <psmdc0714@gmail.com>
2022-08-01 22:12:33 +0900

    220801 solved
    This is the solved problem on 220801

---
+++ c++/2420.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long a, b;
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b;
+    cout << abs(a - b);
+
+    return 0;
+}
