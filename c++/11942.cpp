dec4e5da903fe36d68f263d7d153dbd3207d9759
SangminPark <psmdc0714@gmail.com>
2022-08-27 14:00:47 +0900

    220827 solved
    This is the solved problem on 220827 (easy because of TOEIC)

---
+++ c++/11942.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "고려대학교\n";
+
+    return 0;
+}
